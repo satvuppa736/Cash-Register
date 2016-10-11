a36e0cb4e5bcd5baa9cb48a128356644408aa2c8
satvuppa736 <satvuppa736@ed.amdsb.ca>
2016-10-11 13:31:18 -0400

    First Commit

--- Cash Register/Form1.cs
+++ Cash Register/Form1.cs
@@ -12,6 +12,9 @@ namespace Cash_Register
 {
     public partial class cashRegister : Form
     {
+
+        //Setup Variables 
+
         const double PEP_PIZZA = 2.50;
         const double CHEESE_PIZZA = 2.00;
         const double HW_PIZZA = 3.00;
