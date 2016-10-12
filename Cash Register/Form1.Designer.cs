dbf54b0cb4a61c637b6f7ff9eec59fd24ad35ee0
satvuppa736 <satvuppa736@ed.amdsb.ca>
2016-10-12 13:34:30 -0400

    Commit (October 10th 2016)

--- Cash Register/Form1.Designer.cs
+++ Cash Register/Form1.Designer.cs
@@ -33,15 +33,20 @@
             this.pizzaLabel1 = new System.Windows.Forms.Label();
             this.cheesePizza = new System.Windows.Forms.Label();
             this.canadianPizza = new System.Windows.Forms.Label();
-            this.subtotal = new System.Windows.Forms.Label();
+            this.subtotalLabel = new System.Windows.Forms.Label();
             this.calculateSubtotal = new System.Windows.Forms.Button();
             this.totalButton = new System.Windows.Forms.Button();
             this.totalText = new System.Windows.Forms.Label();
             this.canadianInput = new System.Windows.Forms.TextBox();
-            this.textInput = new System.Windows.Forms.TextBox();
+            this.pepInput = new System.Windows.Forms.TextBox();
             this.cheeseInput = new System.Windows.Forms.TextBox();
             this.subtotalOutput = new System.Windows.Forms.Label();
             this.totalOutput = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.amountInput = new System.Windows.Forms.TextBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.chnageLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -86,15 +91,15 @@
             this.canadianPizza.TabIndex = 3;
             this.canadianPizza.Text = "CANADIAN PIZZA";
             // 
-            // subtotal
+            // subtotalLabel
             // 
-            this.subtotal.AutoSize = true;
-            this.subtotal.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.subtotal.Location = new System.Drawing.Point(12, 291);
-            this.subtotal.Name = "subtotal";
-            this.subtotal.Size = new System.Drawing.Size(120, 26);
-            this.subtotal.TabIndex = 4;
-            this.subtotal.Text = "SUBTOTAL";
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(12, 291);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(120, 26);
+            this.subtotalLabel.TabIndex = 4;
+            this.subtotalLabel.Text = "SUBTOTAL";
             // 
             // calculateSubtotal
             // 
@@ -108,6 +113,7 @@
             this.calculateSubtotal.TabIndex = 5;
             this.calculateSubtotal.Text = "Calculate Subtotal";
             this.calculateSubtotal.UseVisualStyleBackColor = true;
+            this.calculateSubtotal.Click += new System.EventHandler(this.calculateSubtotal_Click);
             // 
             // totalButton
             // 
@@ -121,6 +127,7 @@
             this.totalButton.TabIndex = 6;
             this.totalButton.Text = "Calculate Total";
             this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
             // 
             // totalText
             // 
@@ -134,6 +141,7 @@
             // 
             // canadianInput
             // 
+            this.canadianInput.Font = new System.Drawing.Font("OCR A Std", 11.25F);
             this.canadianInput.Location = new System.Drawing.Point(228, 167);
             this.canadianInput.Multiline = true;
             this.canadianInput.Name = "canadianInput";
@@ -141,17 +149,19 @@
             this.canadianInput.TabIndex = 8;
             this.canadianInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textInput
+            // pepInput
             // 
-            this.textInput.Location = new System.Drawing.Point(228, 72);
-            this.textInput.Multiline = true;
-            this.textInput.Name = "textInput";
-            this.textInput.Size = new System.Drawing.Size(162, 26);
-            this.textInput.TabIndex = 9;
-            this.textInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.pepInput.Font = new System.Drawing.Font("OCR A Std", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pepInput.Location = new System.Drawing.Point(228, 72);
+            this.pepInput.Multiline = true;
+            this.pepInput.Name = "pepInput";
+            this.pepInput.Size = new System.Drawing.Size(162, 26);
+            this.pepInput.TabIndex = 9;
+            this.pepInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // cheeseInput
             // 
+            this.cheeseInput.Font = new System.Drawing.Font("OCR A Std", 11.25F);
             this.cheeseInput.Location = new System.Drawing.Point(228, 119);
             this.cheeseInput.Multiline = true;
             this.cheeseInput.Name = "cheeseInput";
@@ -163,7 +173,7 @@
             // 
             this.subtotalOutput.AutoSize = true;
             this.subtotalOutput.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.subtotalOutput.Location = new System.Drawing.Point(245, 291);
+            this.subtotalOutput.Location = new System.Drawing.Point(223, 291);
             this.subtotalOutput.Name = "subtotalOutput";
             this.subtotalOutput.Size = new System.Drawing.Size(0, 26);
             this.subtotalOutput.TabIndex = 11;
@@ -172,10 +182,63 @@
             // 
             this.totalOutput.AutoSize = true;
             this.totalOutput.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalOutput.Location = new System.Drawing.Point(245, 417);
+            this.totalOutput.Location = new System.Drawing.Point(223, 417);
             this.totalOutput.Name = "totalOutput";
             this.totalOutput.Size = new System.Drawing.Size(0, 26);
             this.totalOutput.TabIndex = 12;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(12, 486);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(123, 26);
+            this.tenderedLabel.TabIndex = 13;
+            this.tenderedLabel.Text = "TENDERED";
+            // 
+            // amountInput
+            // 
+            this.amountInput.Font = new System.Drawing.Font("OCR A Std", 11.25F);
+            this.amountInput.Location = new System.Drawing.Point(228, 486);
+            this.amountInput.Multiline = true;
+            this.amountInput.Name = "amountInput";
+            this.amountInput.Size = new System.Drawing.Size(162, 26);
+            this.amountInput.TabIndex = 14;
+            this.amountInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // changeButton
+            // 
+            this.changeButton.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.changeButton.FlatAppearance.BorderSize = 2;
+            this.changeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.changeButton.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.Location = new System.Drawing.Point(228, 552);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(162, 36);
+            this.changeButton.TabIndex = 15;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // chnageLabel
+            // 
+            this.chnageLabel.AutoSize = true;
+            this.chnageLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chnageLabel.Location = new System.Drawing.Point(12, 621);
+            this.chnageLabel.Name = "chnageLabel";
+            this.chnageLabel.Size = new System.Drawing.Size(101, 26);
+            this.chnageLabel.TabIndex = 16;
+            this.chnageLabel.Text = "CHANGE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(223, 621);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 26);
+            this.label1.TabIndex = 17;
             // 
             // cashRegister
             // 
@@ -183,15 +246,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSpringGreen;
             this.ClientSize = new System.Drawing.Size(850, 671);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chnageLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.amountInput);
+            this.Controls.Add(this.tenderedLabel);
             this.Controls.Add(this.totalOutput);
             this.Controls.Add(this.subtotalOutput);
             this.Controls.Add(this.cheeseInput);
-            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.pepInput);
             this.Controls.Add(this.canadianInput);
             this.Controls.Add(this.totalText);
             this.Controls.Add(this.totalButton);
             this.Controls.Add(this.calculateSubtotal);
-            this.Controls.Add(this.subtotal);
+            this.Controls.Add(this.subtotalLabel);
             this.Controls.Add(this.canadianPizza);
             this.Controls.Add(this.cheesePizza);
             this.Controls.Add(this.pizzaLabel1);
@@ -210,15 +278,20 @@
         private System.Windows.Forms.Label pizzaLabel1;
         private System.Windows.Forms.Label cheesePizza;
         private System.Windows.Forms.Label canadianPizza;
-        private System.Windows.Forms.Label subtotal;
+        private System.Windows.Forms.Label subtotalLabel;
         private System.Windows.Forms.Button calculateSubtotal;
         private System.Windows.Forms.Button totalButton;
         private System.Windows.Forms.Label totalText;
         private System.Windows.Forms.TextBox canadianInput;
-        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.TextBox pepInput;
         private System.Windows.Forms.TextBox cheeseInput;
         private System.Windows.Forms.Label subtotalOutput;
         private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox amountInput;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label chnageLabel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Cash Register/Form1.cs
+++ Cash Register/Form1.cs
@@ -13,18 +13,62 @@ namespace Cash_Register
     public partial class cashRegister : Form
     {
 
-        //Setup Variables 
-        //GAng Gang 
-        const double PEP_PIZZA = 2.50;
-        const double CHEESE_PIZZA = 2.00;
-        const double HW_PIZZA = 3.00;
-
+        //Setup Variables
+        const double PEP_PIZZA_COST = 15.50;
+        const double CHEESE_PIZZA_COST = 10.00;
+        const double CA_PIZZA_COST = 20.00;
+        const double TAX = 1.13;
+        double subtotal;
+        double total;
+        double cash;
+        double change;
+        int pep_pizza;
+        int cheese_pizza;
+        int ca_pizza;
 
         public cashRegister()
         {
             InitializeComponent();
         }
 
-       
+        private void calculateSubtotal_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+
+                //Converts the text input into a number value and places it into a variable
+
+                pep_pizza = Convert.ToInt16(pepInput.Text);
+                cheese_pizza = Convert.ToInt16(cheeseInput.Text);
+                ca_pizza = Convert.ToInt16(canadianInput.Text);
+
+                subtotal = (pep_pizza * PEP_PIZZA_COST) + (cheese_pizza * CHEESE_PIZZA_COST) + (ca_pizza * CA_PIZZA_COST);
+
+                subtotalOutput.Text = subtotal.ToString("C");
+
+            }
+
+
+            catch
+            {
+
+                subtotalOutput.Text = "If you dont want a pizza, enter the value as '0'";
+            }
+                 
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+            total = subtotal * TAX;
+            totalOutput.Text = total.ToString("C");
+
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
